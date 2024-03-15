5db70afc5a5f55e1906d1e283e042cda93411a68
Petter <petter.egesund@gmail.com>
2024-03-16 00:09:04 +0100

    started update layers

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,13 @@
 {-# OPTIONS_GHC -Wno-orphans #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use newtype instead of data" #-}
+{-# HLINT ignore "Use join" #-}
 module Main where
 import Data.Functor
 import Data.List.Split
 import Numeric.LinearAlgebra
 import Data.List
+import System.Random
 
 -- import UnliftIO
 -- import Control.Concurrent
@@ -16,17 +18,19 @@ data AFunction = Relu | Sigmoid | SoftMax | Tanh deriving (Show)
 
 data TrainingData = TrainingData { inputs :: Vector Double, label :: Double } deriving (Show)
 
-data TraininParameters = TraininParameters {
+data TrainingParameters = TraininParameters {
   epochs :: Int,
   batchSize :: Int,
   maxChildren :: Int,
   maxEphochLifes :: Int,
   devSpeed :: Double,
   lossFunction :: Vector Double -> Vector Double -> Double,
-  accuracyFunction :: Vector Double -> Vector Double -> Double }
+  accuracyFunction :: Vector Double -> Vector Double -> Double,
+  changeDir :: Int
+  }
 
-instance Show TraininParameters where
-  show (TraininParameters epochs' batchSize' maxChildren' maxEphochLifes' devSpeed' _ _) = "TraininParameters\n" ++ "epochs: " ++ show epochs' ++ "\n" ++ "batchSize: " ++ show batchSize' ++ "\n" ++ "maxChildren: " ++ show maxChildren' ++ "\n" ++ "maxEphochLifes: " ++ show maxEphochLifes' ++ "\n" ++ "devSpeed: " ++ show devSpeed'
+instance Show TrainingParameters where
+  show (TraininParameters epochs' batchSize' maxChildren' maxEphochLifes' devSpeed' _ _ _) = "TraininParameters\n" ++ "epochs: " ++ show epochs' ++ "\n" ++ "batchSize: " ++ show batchSize' ++ "\n" ++ "maxChildren: " ++ show maxChildren' ++ "\n" ++ "maxEphochLifes: " ++ show maxEphochLifes' ++ "\n" ++ "devSpeed: " ++ show devSpeed'
 
 data Layer = Layer {
   weights :: Vector Double,
@@ -39,7 +43,7 @@ data NeuralNetwork = NeuralNetwork { biases :: Vector Double, layers :: [Layer] 
 data Trainer = Trainer {
   nn :: NeuralNetwork,
   trainingData :: [TrainingData],
-  traininParameters :: TraininParameters } deriving (Show)
+  traininParameters :: TrainingParameters } deriving (Show)
 
 data TrainedNetwork = TrainedNetwork {
   nn' :: NeuralNetwork,
@@ -87,15 +91,14 @@ createLayerWithRandomWeights len aFunction = do
     return $ Layer (flatten weights) (directionOneOrMinusOne $ flatten direction) (flatten directionStength) aFunction
     where directionOneOrMinusOne = cmap (\x -> if x > 0.5 then 1 else -1)
 
-
 createNeuralNetwork :: [(Int, AFunction)] -> IO NeuralNetwork
 createNeuralNetwork layers = do
     biases' <- do flatten <$> rand 1 (fst $ head layers)
     layers' <- mapM (uncurry createLayerWithRandomWeights) layers
-    return $ NeuralNetwork biases' layers' 
+    return $ NeuralNetwork biases' layers'
 
 feedForward :: NeuralNetwork -> Vector Double -> Vector Double
-feedForward (NeuralNetwork biases' layers) indata = foldl' feedForwardLayer indata layers
+feedForward (NeuralNetwork biases' layers) indata = foldl' feedForwardLayer (indata + biases') layers
     where
       feedForwardLayer :: Vector Double -> Layer -> Vector Double
       feedForwardLayer indata' (Layer weights'  _ _ activation') = case activation' of
@@ -104,7 +107,7 @@ feedForward (NeuralNetwork biases' layers) indata = foldl' feedForwardLayer inda
         SoftMax -> sumWeights softMax
         Tanh -> sumWeights tanH
         where sumWeights func = func $ let allWeightsAndBiases theWeight = sumElements $ cmap (+ theWeight) indata'
-                                          in cmap allWeightsAndBiases weights' + biases'
+                                          in cmap allWeightsAndBiases weights'
 
 main :: IO ()
 main = do
@@ -121,11 +124,19 @@ testme = do
 
 
 -- create a function which updates the weights of the network based by randomy changing the weights and biases of the layers 
--- updateWeights :: NeuralNetwork -> TraininParameters -> NeuralNetwork
--- updateWeights nn traininParameters = do
---    let layers' = layers nn
---    let newLayers = map (updateLayerWeights traininParameters) layers'
---    NeuralNetwork newLayers where
---      updateLayerWeights :: TraininParameters -> Layer -> Layer
---      updateLayerWeights traininParameters' (Layer weights' biases' direction' directionStength' activation') = do
+updateWeights :: NeuralNetwork -> TrainingParameters -> IO NeuralNetwork
+updateWeights nn traininParameters = do
+  let layers' = layers nn
+  newLayers <- mapM (updateLayerWeights traininParameters) layers'
+  return $ NeuralNetwork (biases nn) newLayers
+  where
+    updateLayerWeights :: TrainingParameters -> Layer -> IO Layer
+    updateLayerWeights trainingParameters' (Layer weights' direction' directionStength' activation') = do
+      rDir <- randomRIO (0, changeDir trainingParameters') :: IO Int
+      let direction'' = if rDir == 1 then direction' * (-1) else direction'
+      rDirStrength <- randomRIO (-0.1, 0.1) :: IO Double
+      let directionStength'' = cmap (\x -> x * ( 1 + abs rDirStrength)) directionStength' * direction''
+      return $ Layer weights' direction'' directionStength'' activation'
+
+
 
